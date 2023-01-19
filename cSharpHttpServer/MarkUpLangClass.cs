7d1a1fdb23db7a8d82ad34d0c5239f4165c4d1a6
Osbourne L A Clark <osbourne.clark14@gmail.com>
2023-01-19 16:20:52 +0000

    markup lang with markuptoJson. need to add json to markup

--- cSharpHttpServer/MarkUpLangClass.cs
+++ cSharpHttpServer/MarkUpLangClass.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.NetworkInformation;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
+﻿
+
 using System.Text;
-using System.Net;
-using static System.Net.Mime.MediaTypeNames;
-using System.Reflection.Metadata.Ecma335;
-using System.Xml.Linq;
-using System.Security.Cryptography.X509Certificates;
 
 class Element
 {
@@ -20,11 +12,13 @@ class Element
     public List<Element> ChildElements = new List<Element>();
 
     //--constructors--//
-    public Element(string elementName, string insideElement) {
+    public Element(string elementName, string insideElement)
+    {
         this.elementName = elementName;
         this.InsideElement = insideElement;
     }
-    public Element(Element ele) {
+    public Element(Element ele)
+    {
         this.elementName = ele.elementName;
         this.InsideElement = ele.InsideElement;
         this.attributes = ele.attributes;
@@ -40,13 +34,29 @@ class Element
         ChildElements.Add(new Element(elementName, insideElement));
     }
     //deletes first occurence
-    public void deleteChild(string childName) {
-        for (int i = 0; i < ChildElements.Count; i++) {
-            if (ChildElements[i].elementName == childName) {
+    public void deleteChild(string childName)
+    {
+        for (int i = 0; i < ChildElements.Count; i++)
+        {
+            if (ChildElements[i].elementName == childName)
+            {
                 ChildElements.RemoveAt(i);
                 return;
             }
         }
+    }
+
+    public List<Element> getChildren(string childName)
+    {
+        List<Element> children = new List<Element>();
+        foreach (Element child in this.ChildElements)
+        {
+            if (child.elementName == childName)
+            {
+                children.Add(child);
+            }
+        }
+        return children;
     }
 
     //--Attribute CURD(create update read delete)--//
@@ -57,20 +67,23 @@ class Element
         this.attributes.Add(attributeName, value);
     }
     //delete attribute
-    public void deleteAttribute(string attributeName) {
+    public void deleteAttribute(string attributeName)
+    {
         this.attributes.Remove(attributeName);
     }
     //modify attribute (completely rewrites)
     public void modifyAttributeValue(string attributeName, string value)
     {
-        if (!this.attributes.ContainsKey(attributeName)) {
-            addAttribute(attributeName, value); 
+        if (!this.attributes.ContainsKey(attributeName))
+        {
+            addAttribute(attributeName, value);
         }
-        else{ this.attributes[attributeName] = value; }
+        else { this.attributes[attributeName] = value; }
     }
     //get attrubute value, if it doesnt exist return null
-    public string getAttributeValue(string attributeName) {
-        if (!this.attributes.ContainsKey(attributeName)){ return "NULL"; }
+    public string getAttributeValue(string attributeName)
+    {
+        if (!this.attributes.ContainsKey(attributeName)) { return "NULL"; }
         return this.attributes[attributeName];
     }
 
@@ -104,7 +117,8 @@ class Element
         return doc;
     }
     //save elements to file
-    public void SaveToFile(string nameName, string filepath = "./") {
+    public void SaveToFile(string nameName, string filepath = "./")
+    {
         byte[] fileWriter = Encoding.UTF8.GetBytes(constructDoc(debug: true));
         FileStream OpenedFile = File.Open(filepath + nameName, FileMode.Create);
         OpenedFile.Write(fileWriter);
@@ -114,7 +128,7 @@ class Element
 
     //--parseing input--//
     //delimit with "<"
-    public void parseMarkup(List<string> dataSplit)
+    void parseMarkup(List<string> dataSplit)
     {
         if (dataSplit.Count == 0) { return; }
         string operatingDataLine = dataSplit[0];
@@ -148,45 +162,97 @@ class Element
         }
 
         //sets current element name and atributes
-        for (int i = 0; i < nameAndAttributes.Length; i++) {
+        for (int i = 0; i < nameAndAttributes.Length; i++)
+        {
             if (i == 0) { this.elementName = nameAndAttributes[i]; }
             else
             {
                 string[] nameValue = nameAndAttributes[i].Split("=", 2);
                 if (nameValue.Length <= 1)
                 { addAttribute(nameValue[0], "NULL"); }
-                else{ addAttribute(nameValue[0], nameValue[1].Trim('"')); }
+                else { addAttribute(nameValue[0], nameValue[1].Trim('"')); }
             }
         }
 
         //loops till either count is 0 or the end tag is reached
-        while (dataSplit.Count > 0) {
+        while (dataSplit.Count > 0)
+        {
             if (dataSplit[0] == "/" + this.elementName + ">")
             {
                 dataSplit.RemoveAt(0);
                 break;
             }
-            else {
+            else
+            {
                 this.ChildElements.Add(new Element());
                 this.ChildElements[^1].parseMarkup(dataSplit);
             }
         }
     }
     //parse from string
-    public void parseString(string data) {
+    public void parseString(string data)
+    {
         parseMarkup(Element.convertArrayToList(data.Split("<", StringSplitOptions.RemoveEmptyEntries)));
     }
     //parse from file
     //this needs "./"
-    public void parseFile(string fileName) {
+    public void parseFile(string fileName)
+    {
         string singleData = "";
         {
             string[] data = File.ReadAllLines(fileName);
-            foreach (string var in data) {
+            foreach (string var in data)
+            {
                 singleData += var.Trim(new char[] { '\t', ' ', (char)0x09 });
             }
         }
         parseMarkup(Element.convertArrayToList(singleData.Split("<", StringSplitOptions.RemoveEmptyEntries)));
+    }
+
+
+    public string convertMarkupToJson(int index = 0, bool debug = false)
+    {
+        string conjoinedChildrenVals = "";
+        List<string> headNumberOfOccu = new List<string>();
+
+        //if there are no children
+        if (this.ChildElements.Count == 0)
+        {
+            return "\"" + this.InsideElement + "\"";
+        }
+
+        for (int i = 0; i < this.ChildElements.Count; i++)
+        {
+            if (!headNumberOfOccu.Contains(this.ChildElements[i].elementName))
+            {
+                headNumberOfOccu.Add(this.ChildElements[i].elementName);
+            }
+        }
+
+        conjoinedChildrenVals = "{" + endLine(debug);
+        //if there are children
+        for (int i = 0; i < headNumberOfOccu.Count; i++)
+        {
+            List<Element> children = getChildren(headNumberOfOccu[i]);
+            //gets all children values
+            if (children.Count == 1)
+            {
+                conjoinedChildrenVals += indentCreator(index, debug) + "\"" + children[0].elementName + "\"" + ":" + children[0].convertMarkupToJson(index + 1, debug);
+            }
+            else
+            {
+                conjoinedChildrenVals += indentCreator(index, debug) + "\"" + headNumberOfOccu[i] + "\":[" + endLine(debug);
+                for (int j = 0; j < children.Count; j++)
+                {
+                    conjoinedChildrenVals += indentCreator(index, debug) + children[j].convertMarkupToJson(index + 1, debug) + (j == children.Count - 1 ? "" : ",") + endLine(debug);
+                }
+                conjoinedChildrenVals += indentCreator(index, debug) + "]";
+            }
+            conjoinedChildrenVals += (i == headNumberOfOccu.Count - 1 ? "" : ",") + endLine(debug);
+        }
+        conjoinedChildrenVals += "}" + endLine(debug);
+
+        return (index == 0 ? "{" + endLine(debug) + indentCreator(index, debug) + "\"" + this.elementName + "\":" : "") + conjoinedChildrenVals + (index == 0 ? "}" : "");
     }
 
 
@@ -206,15 +272,18 @@ class Element
         return debug ? "\n" : "";
     }
 
-    public static List<string> convertArrayToList(string[] array) { 
+    public static List<string> convertArrayToList(string[] array)
+    {
         List<string> list = new List<string>();
         foreach (string var in array) { list.Add(var); }
         return list;
     }
-    public static string[] MergeArrayIndex(int start, int last, string[] array, string joiningChar = "") {
+    public static string[] MergeArrayIndex(int start, int last, string[] array, string joiningChar = "")
+    {
         List<string> newArray = new List<string>();
         bool boolstart = false;
-        for (int i = 0; i < array.Length; i++) {
+        for (int i = 0; i < array.Length; i++)
+        {
             if (i >= start && i <= last)
             {
                 if (!boolstart) { newArray.Add(array[i]); boolstart = true; }
@@ -231,30 +300,19 @@ class Element
         //dg
         return newArray.ToArray();
     }
-}
 
-class Program
-{
-
-
-
-    public static void Main(string[] args)
+    static int Main()
     {
+        Element test = new Element("root", "");
+        test.createChild("tab1", "test");
+        test.createChild("tab2", "test1");
+        test.createChild("tab2", "test3");
 
-        Element dom = new Element();
-        dom.parseFile("./output.xml");
-        Console.WriteLine("opened from file: \n{0}",dom.constructDoc(debug: true));
+        Console.WriteLine(test.convertMarkupToJson(debug: true));
 
-        dom.ChildElements[0].modifyAttributeValue("att3", "I changed this");
-        dom.ChildElements[0].deleteAttribute("lol");
-        for (int i = 1; i < dom.ChildElements.Count; i++) {
-            dom.ChildElements[i].InsideElement = "";
-        }
 
-        Console.WriteLine("modifyed from file: \n{0}", dom.constructDoc(debug: true));
-        dom.SaveToFile("testoutput.xml");
-
-      
-
+        return 0;
     }
+
 }
+
